642392d7f8d3e8cfb846385e7a6b53859e421800
Julian Shah <julakshah@gmail.com>
2025-12-12 09:46:50 -0500

    cleaning up launch files and adding the mediapipe task

---
+++ ros2_ws/src/chair_robot/launch/launch_trash_robot.py
@@ -0,0 +1,23 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='twist_mux',
+            exectuable='twist_mux',
+            name='twist_mux',
+            parameters=['config/twist_mux.yaml']
+        )
+        Node(
+            package='chair_robot',
+            exectuable='transform_helper',
+            name='transform_helper'
+            )
+        Node(
+            package='chair_robot',
+            exectuable='robot_state',
+            name='state_machine'
+            parameters=[{'is_trashcan': True}]
+            )
+        ])
