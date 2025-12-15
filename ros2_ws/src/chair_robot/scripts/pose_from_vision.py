c4790a44dba1b4c99637693cd03fceab46842ed8
Ben Ricket <ben.ricket@gmail.com>
2025-12-14 20:00:15 -0500

    Downsample vision script to run on pi

--- ros2_ws/src/chair_robot/scripts/pose_from_vision.py
+++ ros2_ws/src/chair_robot/scripts/pose_from_vision.py
@@ -37,12 +37,14 @@ VisionRunningMode = mp.tasks.vision.RunningMode
 
 class PoseFromVision(Node):
     
-    def __init__(self):
+    def __init__(self,channel):
         super().__init__('pose_from_vision')
 
         self.aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_100)
         self.detect_params = cv2.aruco.DetectorParameters()
         self.detector = cv2.aruco.ArucoDetector(self.aruco_dict,detectorParams=self.detect_params)
+
+        self.channel = channel
 
         self.camMatrix = RS_INTRINSIC_COLOR_640
         self.distCoeffs = RS_DIST_COLOR_640
@@ -54,16 +56,28 @@ class PoseFromVision(Node):
         self.cap = self.create_cap(attempt=0)
 
         # For testing
-        self.timer = self.create_timer(.2, self.detect)
+        self.timer = self.create_timer(0.01, self.detect)
 
     def detect(self):
+        # flush buffer --- we want to make sure we grab a recent frame
+        for _ in range(3):
+            self.cap.grab()
+
         # pull frame from cv2
-        _, frame = self.cap.read()
-        print(f"frame is {frame}")
+        ret, frame = self.cap.read()
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            del(self)
+        
         frame = cv2.flip(frame, 1)  # pylint: disable=no-member
+        if not ret:
+            print("failed to get frame")
+            return
+        #print(f"Frame shape: {frame.shape}")
 
         # draw the landmarks on the page for visualization
         landmarked_frame = draw_landmarks_on_image(frame, self.cv_result)
+        #print(f"Type of landmarked image: {type(landmarked_frame)}")
+        #print(f"Landmarked frame shape: {landmarked_frame.shape}")
         cv2.imshow("frame", landmarked_frame)
         if frame is not None:
             mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
@@ -79,9 +93,14 @@ class PoseFromVision(Node):
         Args:
             attempt: an integer representing the current attempt
         """
-        cap = cv2.VideoCapture(0)  # pylint: disable=no-member
+        print(f"self.channel: {self.channel}")
+        cap = cv2.VideoCapture(self.channel)  # pylint: disable=no-member
         time.sleep(0.5)
         if cap.isOpened():
+            cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+            cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+            cap.set(cv2.CAP_PROP_FPS, 15)
+            cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
             return cap
         elif attempt == 10:
             print("video capture FAILED, closing")
@@ -125,27 +144,38 @@ class PoseFromVision(Node):
 
 
 def draw_landmarks_on_image(rgb_image, detection_result):
-  pose_landmarks_list = detection_result.pose_landmarks
-  annotated_image = np.copy(rgb_image)
+    pose_landmarks_list = detection_result.pose_landmarks
+    annotated_image = np.copy(rgb_image)
+    #print(f"Annotated image at start of draw landmarks on image: {type(annotated_image)}")
 
-  # Loop through the detected poses to visualize.
-  for idx in range(len(pose_landmarks_list)):
-    pose_landmarks = pose_landmarks_list[idx]
+    # Loop through the detected poses to visualize.
+    pose_landmarks = []
+    # Check for empty pose list
+    if len(pose_landmarks_list) == 0:
+        return annotated_image
 
+    for idx in range(len(pose_landmarks_list)):
+        pose_landmarks += pose_landmarks_list[idx]
+    
     # Draw the pose landmarks.
     pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
     pose_landmarks_proto.landmark.extend([
-      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
+        landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
     ])
     solutions.drawing_utils.draw_landmarks(
-      annotated_image,
-      pose_landmarks_proto,
-      solutions.pose.POSE_CONNECTIONS,
-      solutions.drawing_styles.get_default_pose_landmarks_style())
-  return annotated_image
+        annotated_image,
+        pose_landmarks_proto,
+        solutions.pose.POSE_CONNECTIONS,
+        solutions.drawing_styles.get_default_pose_landmarks_style())
+    #print(f"Type of annotated image at end of draw landmarks on image: {type(annotated_image)}")
+    return annotated_image
 
 if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        channel = 0
+    else:
+        channel = int(sys.argv[1])
     rclpy.init()
-    pose_from_vision = PoseFromVision()
+    pose_from_vision = PoseFromVision(channel=channel)
     rclpy.spin(pose_from_vision)
     rclpy.shutdown()
--- run_container.sh
+++ run_container.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/sh
 docker run -it --rm $(for d in /dev/video*; do printf -- '--device=%s ' "$d"; done) \
     $(for d in /dev/gpiochip*; do printf -- "--device %s:%s " "$d" "$d"; done) \
-    -e ROS_DOMAIN_ID=99 --network=host --ipc=host --pid=host desk-on-demand bash
+    -e ROS_DOMAIN_ID=99 --network=host --ipc=host --pid=host \
+    -e DISPLAY="$DISPLAY" \
+    -e XAUTHORITY=/tmp/.Xauthority \
+    -v "$HOME/.Xauthority:/tmp/.Xauthority:ro" \ 
+    desk-on-demand bash
