176394b1ab08c4bc85db10aa49c9cbaa1f7daaeb
Ben Ricket <ben.ricket@gmail.com>
2025-12-16 20:27:54 -0500

    Add launch script including lidar

---
+++ ros2_ws/src/chair_robot/launch/chair_lidar.launch.py
@@ -0,0 +1,39 @@
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, OpaqueFunction, IncludeLaunchDescription
+
+def launch_setup(context):
+    video_device_str = LaunchConfiguration("device").perform(context)
+    video_device = int(video_device_str)
+
+    nodes_to_launch = [
+        Node(
+            package='chair_robot',
+            executable='robot_state.py',
+            name='robot0',
+            namespace='robot0',
+            parameters=[
+                {"device":video_device},
+                {"is_trashcan":False},
+                {"follow_id":-1}
+            ]
+        ),
+        Node(
+            package='chair_robot',
+            executable='transform_helper.py',
+            name='static_broadcast',
+        )
+    ]
+    
+    return nodes_to_launch
+
+def generate_launch_description():
+    video_device_arg = DeclareLaunchArgument(
+        "device",
+        default_value='4',
+        description="ID of video device to use"
+    )
+    # Call launch file to initiate lidar
+    lidar_launch = launch.actions.IncludeLaunchDescription(package='sllidar_ros2', launch='sllidar_a1.launch.py')  
+    return LaunchDescription([video_device_arg, OpaqueFunction(function=launch_setup),lidar_launch])
